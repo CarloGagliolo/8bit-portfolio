9c9ff03f93c6be23f5efcaa45d19bda8f3b2ebef
carlo_gagliolo <carlo126055@gmail.com>
2025-10-23 15:58:39 +0200

    Removed link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (    
       <main className="flex flex-col items-center justify-between p-24">     
-        <div className="flex">
-          <a className="nes-btn" href="#">Normal</a>
+        <div className="flex">          
           <button type="button" className="nes-btn is-primary">Primary</button>
           <button type="button" className="nes-btn is-success">Success</button>
           <button type="button" className="nes-btn is-warning">Warning</button>
