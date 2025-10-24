4fab3a0b57cb0c0fe5604cd2fc58e32440e2594f
carlo_gagliolo <carlo126055@gmail.com>
2025-10-24 08:53:22 +0200

    Added grass background

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 export default function Home() {
   return (    
-      <main className="flex flex-col items-center justify-between p-24">     
-        <div className="flex">          
-          <button type="button" className="nes-btn is-primary">Primary</button>
-          <button type="button" className="nes-btn is-success">Success</button>
-          <button type="button" className="nes-btn is-warning">Warning</button>
-          <button type="button" className="nes-btn is-error">Error</button>
-          <button type="button" className="nes-btn is-disabled">Disabled</button>
-          <label className="nes-btn">
-            <span>Select your file</span>
-          </label>
-        </div>   
+      <main >
+        <div className="flex flex-col items-center justify-between pt-24 gap-10">
+        <div  className="text-center">
+          <h1>Stai cercando un vero e proprio Dev Warrior?</h1>            
+        </div>
+        
+        <div className="nes-container with-title is-centered bg-blue-200 w-[45rem]">
+          <p className="title">Greetings</p>
+          <p>
+            Sono uno sviluppatore web da piu di 10 anni.
+            Ho un sacco di storie interessanti da raccontare.
+
+          </p>
+        </div>
+        </div>
+       
       </main>    
   );
 }
